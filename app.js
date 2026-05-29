f7b32a7d19f3d43ede1cf9c7cd7dd0cc15e62ec3
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-05-29 02:18:28 +0100

    fix: remove deleted asset app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// app.js
-// Namecheap hPanel startup file. It boots JITI to run server.ts on the fly.
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const jiti = require('jiti')(import.meta.url);
-jiti('./server.ts');
