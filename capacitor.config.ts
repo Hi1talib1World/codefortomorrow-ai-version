4216f5de14f078570139b93517fadc603e57bf25
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-05-29 02:29:16 +0100

    feat: update capacitor logic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.codefortomorrow.app',
   appName: 'code-for-tomorrow-morocco',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
