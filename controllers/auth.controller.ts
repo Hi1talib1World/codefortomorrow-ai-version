90ebe641bb814451304e1713a17732d4e4846394
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-05-31 03:59:05 +0100

    feat: update auth logic

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -13,7 +13,7 @@ const setAuthCookie = (res: Response, token: string) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
