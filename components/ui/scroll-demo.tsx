2673647eb93210943b45ac6faf245e0af778c1d3
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-14 19:22:48 +0100

    feat: update scroll-demo logic

--- components/ui/scroll-demo.tsx
+++ components/ui/scroll-demo.tsx
@@ -6,16 +6,7 @@ export function HeroScrollDemo() {
   return (
     <div className="flex flex-col overflow-hidden pb-12 pt-4 bg-slate-900 text-white">
       <ContainerScroll
-        titleComponent={
-          <div className="mb-6 md:mb-12">
-            <h1 className="text-3xl md:text-5xl font-semibold text-slate-300">
-              Unleash the power of <br />
-              <span className="text-4xl md:text-[5.5rem] font-bold mt-2 leading-none text-[#FBBF24]">
-                Scroll Animations
-              </span>
-            </h1>
-          </div>
-        }
+        titleComponent={""}
       >
         <img
           src="https://ui.aceternity.com/_next/image?url=%2Flinear.webp&w=3840&q=75"
